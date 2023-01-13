b086f0c1a594ef8feda675674d641b6340de6018
limeorannge <insoo0327@snu.ac.kr>
2023-01-13 13:17:11 +0900

    map where

--- mainmap.js
+++ mainmap.js
@@ -1,6 +1,6 @@
 var container = document.getElementById('map');
 var options = {
-    center: new kakao.maps.LatLng(33.450701, 126.570667),
+    center: new kakao.maps.LatLng(37.514255454464006, 127.06022318956269),
     level: 3
 };
 
